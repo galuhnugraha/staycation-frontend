358a4378043ded2ecbf34b51e7981ef05e4e7f3c
Galuh Nugraha <galuhnugraha28@gmail.com>
2020-10-05 15:32:04 +0700

    updates header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import LandingPage from 'pages/LandingPage';
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import "./assets/scss/style.scss";
 
 function App() {
   return (
     <div className="App">
-        <h1>Halo</h1>
+        <Router>
+          <Route path="/" component={LandingPage}></Route>
+        </Router>
     </div>
   );
 }

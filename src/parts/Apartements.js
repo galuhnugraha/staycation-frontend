fd16b40c3740a27f4e95e7fe41c70d5884b9b391
Galuh Nugraha <galuhnugraha28@gmail.com>
2020-10-06 12:54:53 +0700

    updates mostpicked

---
+++ src/parts/Apartements.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Apartements() {
+    return (
+        <section className="container">
+            <h4 className="mb-4">Apartements with kitchen set</h4>
+            <div className="container-grid">
+                <div className="item column-3 row-1">
+                    <div className="card">Card 1 </div>
+                </div>
+                <div className="item column-3 row-1">
+                    <div className="card">Card 2 </div>
+                </div>
+                <div className="item column-3 row-1">
+                    <div className="card">Card 3 </div>
+                </div>
+                <div className="item column-3 row-1">
+                    <div className="card">Card 4 </div>
+                </div>
+            </div>
+        </section >
+    )
+}

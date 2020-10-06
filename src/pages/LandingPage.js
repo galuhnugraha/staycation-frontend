cf7ae1eb6b706efafbfc08af1c720d1b9b8d2109
Galuh Nugraha <galuhnugraha28@gmail.com>
2020-10-06 19:51:52 +0700

    updates footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,8 @@ import landingPage from "json/landingPage.json";
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/mostPicked';
 import Categories from 'parts/Categories';
+import Testimoni from 'parts/Testimoni';
+import Footer from 'parts/Footer'
 
 export default class LandingPage extends Component {
     constructor(props) {
@@ -17,6 +19,8 @@ export default class LandingPage extends Component {
                 <Hero refMostPicked={this.refMostPicked} data={landingPage.hero}/>
                 <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked}/>
                 <Categories data={landingPage.categories}/>
+                <Testimoni data={landingPage.testimonial}/>
+                <Footer/>
             </>
         )
     }
